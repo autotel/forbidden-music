c85514ba91d50f7af3f4abbf04ed4a1039de5a03
Joaquín Aldunate <joaquin.aldunate@gmail.com>
2022-12-26 18:20:03 -0300

    improve col synth

--- src/synth/CollisionSynth.ts
+++ src/synth/CollisionSynth.ts
@@ -10,47 +10,49 @@ class CollisionVoice implements Voice {
     audioContext: AudioContext;
 
     gainNode: GainNode;
-    noise?: AudioWorkletNode;
+    noise: AudioWorkletNode;
 
-    filterNode1: BiquadFilterNode;
-    filterNode2: BiquadFilterNode;
-    filterNode3: BiquadFilterNode;
-    filterNode4: BiquadFilterNode;
+    filterNodes: BiquadFilterNode[];
+    filterAmps: GainNode[];
 
     constructor(audioContext: AudioContext, destination: AudioNode) {
         this.audioContext = audioContext;
         this.inUse = false;
 
-        this.filterNode1 = audioContext.createBiquadFilter();
-        this.filterNode2 = audioContext.createBiquadFilter();
-        this.filterNode3 = audioContext.createBiquadFilter();
-        this.filterNode4 = audioContext.createBiquadFilter();
+        this.filterNodes = [
+            audioContext.createBiquadFilter(),
+            audioContext.createBiquadFilter(),
+            audioContext.createBiquadFilter(),
+            audioContext.createBiquadFilter(),
+            audioContext.createBiquadFilter(),
+            audioContext.createBiquadFilter(),
+            audioContext.createBiquadFilter(),
+            audioContext.createBiquadFilter(),
+        ]
+
+        this.filterAmps = this.filterNodes.map(f => audioContext.createGain());
 
         this.gainNode = audioContext.createGain();
-        this.filterNode1.connect(this.gainNode);
-        this.filterNode2.connect(this.gainNode);
-        this.filterNode3.connect(this.gainNode);
-        this.filterNode4.connect(this.gainNode);
         this.gainNode.connect(destination);
 
         workletPromise.then((noise) => {
             console.log("noise", noise);
             this.noise = noise;
-            this.noise.connect(this.filterNode1);
-            this.noise.connect(this.filterNode2);
-            this.noise.connect(this.filterNode3);
-            this.noise.connect(this.filterNode4);
-            // this.noise.connect(this.gainNode);
+            this.filterNodes.forEach((filter, i) => {
+                this.noise.connect(filter);
+                filter.connect(this.filterAmps[i]);
+                this.filterAmps[i].connect(this.gainNode);
+            });
         });
 
-        this.filterNode1.Q.value = 2;
-        this.filterNode1.frequency.value = 2000;
-        this.filterNode2.Q.value = 3;
-        this.filterNode2.frequency.value = 2000;
-        this.filterNode3.Q.value = 2;
-        this.filterNode3.frequency.value = 2000;
-        this.filterNode4.Q.value = 2;
-        this.filterNode4.frequency.value = 2000;
+
+        const qvals = this.filterNodes.map((f, i) => 11 + 2 ** (5 - i));
+        const ampvals = this.filterNodes.map((f, i) => 2 ** (1 - i));
+
+        console.log(...qvals);
+        console.log(...ampvals);
+        this.filterNodes.map((f, i) => f.Q.value = qvals[i]);
+        this.filterAmps.map((f, i) => f.gain.value = ampvals[i]);
 
     }
 
@@ -62,35 +64,28 @@ class CollisionVoice implements Voice {
         // reset stuff
         this.gainNode.gain.cancelScheduledValues(now - 0.01);
 
-        this.filterNode1.Q.cancelScheduledValues(now - 0.01);
-        this.filterNode2.Q.cancelScheduledValues(now - 0.01);
-        this.filterNode3.Q.cancelScheduledValues(now - 0.01);
-        this.filterNode4.Q.cancelScheduledValues(now - 0.01);
+        this.filterNodes.forEach(f => f.frequency.cancelScheduledValues(now - 0.01));
 
         // set note start values
         this.inUse = true;
         // redundant to ensure no negative values after exponential ramp
         this.gainNode.gain.value = velocity;
-        this.gainNode.gain.setValueAtTime(velocity, now - 0.01);
+        this.gainNode.gain.setValueAtTime(velocity, now - 0.1);
 
         // schedule attack
-        // this.filterNode1.frequency.linearRampToValueAtTime(frequency, now + 0.1);
-        // this.filterNode2.frequency.linearRampToValueAtTime(frequency, now + 0.1);
-        // this.filterNode3.frequency.linearRampToValueAtTime(frequency, now + 0.1);
-        // this.filterNode4.frequency.linearRampToValueAtTime(frequency, now + 0.1);
+        this.filterNodes[0].frequency.value = frequency;
+        this.filterNodes[1].frequency.value = frequency * 4;
+        this.filterNodes[2].frequency.value = frequency * 8;
+        this.filterNodes[3].frequency.value = frequency / 2;
+        this.filterNodes[4].frequency.value = frequency / 4;
+        this.filterNodes[5].frequency.value = frequency * 16;
+        this.filterNodes[6].frequency.value = frequency + 411;
+        this.filterNodes[7].frequency.value = frequency + 111;
 
-        this.filterNode1.frequency.value = frequency * 16;
-        this.filterNode2.frequency.value = frequency;
-        this.filterNode3.frequency.value = frequency * 8;
-        this.filterNode4.frequency.value = frequency / 8;
-
+        // this.filterNodes.forEach((f, i) => f.Q.exponentialRampToValueAtTime(qvals[i], now + 0.1));
         // schedule decay
         this.gainNode.gain.exponentialRampToValueAtTime(velocity * 0.1, now + 0.5);
 
-        this.filterNode1.Q.exponentialRampToValueAtTime(35, now + 0.01);
-        this.filterNode2.Q.exponentialRampToValueAtTime(35, now + 0.01);
-        this.filterNode3.Q.exponentialRampToValueAtTime(35, now + 0.01);
-        this.filterNode4.Q.exponentialRampToValueAtTime(35, now + 0.01);
 
     }
 
@@ -101,11 +96,11 @@ class CollisionVoice implements Voice {
 
         this.gainNode.gain.linearRampToValueAtTime(0, endTimeSeconds + 0.3);
 
-        this.filterNode1.frequency.cancelScheduledValues(0);
-        this.filterNode2.frequency.cancelScheduledValues(0);
-        this.filterNode3.frequency.cancelScheduledValues(0);
-        this.filterNode4.frequency.cancelScheduledValues(0);
-
+        // cancel all filter scheduled values
+        this.filterNodes.forEach(f => {
+            f.Q.cancelScheduledValues(0);
+            f.frequency.cancelScheduledValues(0)
+        });
 
         setTimeout(() => {
             this.inUse = false;
