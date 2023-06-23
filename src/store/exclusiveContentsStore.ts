6851f9a4cd70bbd5d01aa70984c053806b69156b
Joaquín Aldunate <joaquin.aldunate@gmail.com>
2023-06-23 15:46:46 -0400

    make dl version include "exclusives"

--- src/store/exclusiveContentsStore.ts
+++ src/store/exclusiveContentsStore.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useExclusiveContentsStore = defineStore("exclusive contents store", () => {
-    const enabled = ref(false);
+    const enabled = ref(true);
     if(window.location.hash == "#exclusive") {
         enabled.value = true;
         console.log("exclusives mode on");
