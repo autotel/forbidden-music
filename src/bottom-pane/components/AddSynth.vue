008e9016347a5e3aefdc17cce7386036a5f154bd
Joaquín Aldunate <joaquin.aldunate@gmail.com>
2024-06-12 15:12:08 -0400

    add synth graphic improvement

--- src/bottom-pane/components/AddSynth.vue
+++ src/bottom-pane/components/AddSynth.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 
 import { ref } from 'vue';
+import Button from '../../components/Button.vue';
 import { useBottomPaneStateStore } from '../../store/bottomPaneStateStore';
 import { SynthConstructorWrapper, useSynthStore } from '../../store/synthStore';
 import SynthSelector from '../SynthSelector.vue';
-import ModuleContainer from './ModuleContainer.vue';
-import Button from '../../components/Button.vue';
 const synthStore = useSynthStore();
 const expanded = ref(false);
 const bottomPaneState = useBottomPaneStateStore();
@@ -15,21 +14,46 @@ const addSynth = (synth: SynthConstructorWrapper) => {
         bottomPaneState.activeLayerChannel,
         synth
     );
+    expanded.value = false;
 }
 </script>
 <template>
-    <ModuleContainer title="Add" :style="{width:expanded?'12em':0, backgroundColor: 'transparent'}">
-        <template #icons>
+    <div :style="{
+        width:expanded?'auto':'0.1em',
+        backgroundColor: 'transparent',
+        position:'relative',
+        display:'flex',
+        flexDirection:'row',
+        height:'18em'
+    }">
+        <div id="icons">
             <Button 
                 style="background-color: transparent;"    
                 @click="expanded=!expanded"
             >{{expanded?'×':'+'}}
             </Button>
-        </template>
-        <template #default>
+        </div>
+        <div id="selector" v-if="expanded">
             <div style="height:100%; overflow-y: auto;">
                 <SynthSelector @select="addSynth" />
             </div>
-        </template>
-    </ModuleContainer>
+        </div>
+    </div>
 </template>
+<style scoped>
+#icons {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 0.05em;
+    flex-grow: 0;
+    flex-shrink:0;
+}
+#selector {
+    overflow-y: auto;
+    height: 100%;
+    flex-grow: 0;
+    flex-shrink:0;
+}
+</style>
