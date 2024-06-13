95a10ae1442dcf2815bebac99f0144ce876c2dc8
Joaquín Aldunate <joaquin.aldunate@gmail.com>
2024-06-13 16:51:56 -0400

    synth/fx find

--- src/bottom-pane/SynthSelector.vue
+++ src/bottom-pane/SynthSelector.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed, onMounted, ref } from 'vue';
 import Button from '../components/Button.vue';
 import { SynthConstructorWrapper, useSynthStore } from '../store/synthStore';
 
@@ -6,10 +7,18 @@ const synth = useSynthStore();
 defineEmits<{
    (e: 'select', scw: SynthConstructorWrapper): void
 }>();
+const searchString = ref('');
+const searchResults = computed<SynthConstructorWrapper[]>(()=>{
+    return synth.synthConstructorWrappers.filter(scw => scw.name.toLowerCase().includes(searchString.value.toLowerCase()));
+});
+
+
+
 </script>
 <template>
     <h3 class="padded">Synth select</h3>
-    <template v-for="(scw, no) in synth.synthConstructorWrappers">
+    <input v-model="searchString" placeholder="Find" class="padded" style="width:calc(100% - 2em); padding: 0.3em;"/>
+    <template v-for="(scw, no) in searchResults">
         <Button :onClick="() => $emit('select', scw)"
             style="width:calc(100% - 2em); display:flex; justify-content: space-between;" class="padded">
             {{ scw.name }}
