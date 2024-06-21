7199a099d4814258bf8ec5bd5844b14637c69333
Joaquín Aldunate <joaquin.aldunate@gmail.com>
2024-06-20 20:39:58 -0400

    make ringmod decent

--- src/synth/RingModEffect.ts
+++ src/synth/RingModEffect.ts
@@ -14,13 +14,18 @@ export class RingModEffect extends AudioModule {
         super();
         this.output = audioContext.createGain();
         this.input = audioContext.createGain();
-
         const dry = audioContext.createGain();
+        const wet = audioContext.createGain();
+        const ringer = audioContext.createGain();
         dry.gain.value = 0;
+        wet.gain.value = 1;
         const oscillator = audioContext.createOscillator();
         // Id' like to have noise too, but its not available. Later I could think of using periodic wave?
         oscillator.type = "sine";
-
+        type OscillatorOption = {
+            value: OscillatorType,
+            displayName: string,
+        }
         this.params = [{
             type: ParamType.number,
             displayName: "Frequency",
@@ -40,24 +45,48 @@ export class RingModEffect extends AudioModule {
             exportable: true,
         },{
             type: ParamType.number,
-            displayName: "Dry gain",
+            displayName: "wet",
             set value(v: number) {
-                dry.gain.value = v;
+                dry.gain.value = 1-v;
+                wet.gain.value = v;
             },
             get value() {
-                return dry.gain.value;
+                return wet.gain.value;
             },
             min: 0,
             max: 1,
             exportable: true,
         
+        },{
+            type: ParamType.option,
+            displayName: "Waveform",
+            _v: 0,
+            options: [
+                {value: "sine", displayName: "Sine"},
+                {value: "square", displayName: "Square"},
+                {value: "sawtooth", displayName: "Sawtooth"},
+                {value: "triangle", displayName: "Triangle"},
+            ] as OscillatorOption[],
+            set value(v: number) {
+                this._v = v;
+                oscillator.type = this.options[this._v].value as unknown as OscillatorType;
+            },
+            get value() {
+                return this._v;
+            },
+            exportable: true,
         }];
 
         this.enable = async () => {
-            this.input.connect(this.output);
-            oscillator.connect(this.input.gain);
             this.input.connect(dry);
+            this.input.connect(ringer);
+
+            oscillator.connect(ringer.gain);
+            ringer.connect(wet)
+            
             dry.connect(this.output);
+            wet.connect(this.output);
+
             oscillator.start();
         }
 
