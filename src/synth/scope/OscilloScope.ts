3f88d54d1ae24e6dd45082f1902704743ef611d9
Joaquín Aldunate <joaquin.aldunate@gmail.com>
2024-07-17 22:47:23 -0400

    improve oscilloscope

--- src/synth/scope/OscilloScope.ts
+++ src/synth/scope/OscilloScope.ts
@@ -2,11 +2,28 @@ import { createAudioEnvelopeWorklet } from "../../functions/audioEnvelopeWorklet
 import { createMaximizerWorklet } from "../../functions/maximizerWorkletFactory";
 import { AudioModule } from "../types/AudioModule";
 import { createAutomatableAudioNodeParam } from "../types/Automatable";
+import { NumberSynthParam, ParamType, SynthParam } from "../types/SynthParam";
 
 export class OscilloScope extends AudioModule {
     output: GainNode;
     input: GainNode;
     enabledCalled: boolean = false;
+    bufferLengthParam: NumberSynthParam = {
+        type: ParamType.number,
+        displayName: "Zoom",
+        range: [128, 2048],
+        _v: 1024,
+        set value(v: number) {
+            this._v = Math.round(v);
+        },
+        get value() {
+            return this._v;
+        },
+        min: 128,
+        max: 2048,
+        exportable: true,
+    }
+    params: SynthParam[] = [this.bufferLengthParam];
     constructor(
         audioContext: AudioContext,
     ) {
@@ -22,9 +39,10 @@ export class OscilloScope extends AudioModule {
         this.input.connect(analyzer);
         this.input.connect(this.output);
 
-        const returnLength = 1024;
-
+        let prevValue = 0;
+        
         this.getWaveform = () => {
+            const returnLength = this.bufferLengthParam.value;
             const bufferLength = analyzer.frequencyBinCount;
             const dataArray = new Uint8Array(bufferLength);
             let searchUntil = bufferLength - returnLength;
@@ -32,10 +50,14 @@ export class OscilloScope extends AudioModule {
             let triggerIndex = 0;
             analyzer.getByteTimeDomainData(dataArray);
             for(let i = 0; i < searchUntil; i++) {
-                if(dataArray[i] > highestValue) {
+                if(
+                    dataArray[i] > highestValue 
+                    && dataArray[i] > prevValue // on incresing edge
+                ) {
                     highestValue = dataArray[i];
                     triggerIndex = i;
                 }
+                prevValue = dataArray[i];
             }
             return Array.from(dataArray.slice(triggerIndex, triggerIndex + returnLength));
         }
