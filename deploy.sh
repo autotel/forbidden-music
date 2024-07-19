229df2732cf9b5b9367d85d34a0c1671956dd075
Joaquín Aldunate <joaquin.aldunate@gmail.com>
2024-07-19 13:02:42 -0400

    merge to main when deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,29 @@
-npm install;
-npm run build;
-npm run docs:build;
-mkdir ./forbidden-music/docs/;
-cp ./docs/.vuepress/dist/* ./forbidden-music/docs/ -r;
-echo "will run tests now, press ctrl+c to cancel";
-sleep 5;
-npm run test;
+
+
+current_branch=$(git branch --show-current);
+
+read -p "Will merge $current_branch into main. Contiune?" -n 1 -r
+echo    # move to a new line
+
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+git checkout main;
+git pull;
+git merge $current_branch;
+git push;
+
+# npm install;
+# npm run build;
+
+# npm run docs:build;
+# mkdir ./forbidden-music/docs/;
+# cp ./docs/.vuepress/dist/* ./forbidden-music/docs/ -r;
+# echo "will run tests now, press ctrl+c to cancel";
+# sleep 5;
+# npm run test;
+
+git checkout $current_branch;
+
+fi
