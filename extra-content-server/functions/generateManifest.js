5c12ceb3698f5a4b1e1a0a028fd339c91c11697b
Joaquín Aldunate <joaquin.aldunate@gmail.com>
2024-08-27 19:58:07 -0400

    move uni_iowa

--- extra-content-server/functions/generateManifest.js
+++ extra-content-server/functions/generateManifest.js
@@ -9,7 +9,7 @@ const publichPath = 'public';
  * @property {string} pattern
  * @property {string} maps
  */
-
+const audioExtensionPattern = /.(wav|aiff)$/;
 /**
  * @param {DirItem[]} dirsList
  * @param {string} remoteBaseUrl
@@ -37,26 +37,42 @@ const generateSamplesList = (dirsList, remoteBaseUrl) => {
         });
         for (let j = 0; j < files.length; j++) {
             const fileName = files[j];
-            let parsed = parser(fileName);
+            const nameWithoutExtension = fileName.replace(audioExtensionPattern, '');
 
-            if (!parsed.libraryName) {
-                console.error(`No libraryName for ${fileName}`);
+            if (!fileName.match(audioExtensionPattern)) {
+                if (fileName === "readme.txt") {
+                    const library = getLibrary(dirItem.path);
+                    const readmePath = path.join(dirItem.path, fileName);
+                    const readme = fs.readFileSync(path.join(publichPath, readmePath), 'utf8');
+                    library.readme = readme;
+                } else {
+                    console.log("ignoring", fileName);
+                }
                 continue;
             }
+            try {
+                let parsed = parser(nameWithoutExtension);
 
-            if (!parsed.frequency){
-                if('cdeNote' in parsed){
-                    parsed.frequency = cdeNoteToFrequency(parsed.cdeNote);
-                } else {
-                    console.error(`No frequency or cdeNote for ${fileName}`);
-                    continue;
+                if (!parsed.libraryName) {
+                    parsed.libraryName = dirItem.path;
                 }
+
+                if (!parsed.frequency) {
+                    if ('cdeNote' in parsed) {
+                        parsed.frequency = cdeNoteToFrequency(parsed.cdeNote);
+                    } else {
+                        console.error(`No frequency or cdeNote for ${fileName}`);
+                        continue;
+                    }
+                }
+
+                parsed.path = remoteBaseUrl + '/' + path.join(dirItem.path, fileName);
+
+                const library = getLibrary(parsed.libraryName);
+                library.samples.push(parsed);
+            } catch (e) {
+                console.error(`Error parsing ${fileName} as ${nameWithoutExtension}: ${e.message}`);
             }
-
-            parsed.path = remoteBaseUrl+'/'+path.join(dirItem.path, fileName);
-
-            const library = getLibrary(parsed.libraryName);
-            library.samples.push(parsed);
         }
     }
     return libraries;
